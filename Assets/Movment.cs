862f79dc7409f20883f6767fa5208054e6cdde7e
Baggel <83385540+Baggel@users.noreply.github.com>
2022-05-06 12:41:05 +1000

    run done jump start
    finished run animation and started the jump anmation

--- Assets/Movment.cs
+++ Assets/Movment.cs
@@ -8,12 +8,16 @@ using UnityEngine.UI;
     public float MovmentSpeed = 1;
     public float JumpForce = 1;
     private bool CanDoubleJump;
-
+    public Animator animator;
     private Rigidbody2D _rigidbody2D;
+
+    
+    
     
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        
     }
 
     
@@ -22,20 +26,34 @@ using UnityEngine.UI;
         var Movment = Input.GetAxis("Horizontal");
         transform.position += new Vector3(Movment, 0, 0) * Time.deltaTime * MovmentSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(Movment));
+        
+
+
 
         if (!Mathf.Approximately(0, Movment))
             transform.rotation = Movment > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+
 
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody2D.velocity.y) < 0.001f)
         {
             _rigidbody2D.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
             CanDoubleJump = true;
+            animator.SetBool("IsJumping", true);
+
         }
+        if (Mathf.Abs(_rigidbody2D.velocity.y) > 0.001f)
+        {
+            animator.SetBool("IsJumping", false);
+        }
+
+        
 
         else if ( CanDoubleJump && Input.GetButtonDown("Jump"))
         {
             _rigidbody2D.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
             CanDoubleJump = false;
+            
         }
 
 
